be9d0b6b84156143a9805c80e3f0ef21a6458ba3
minnu123456 <86703716+minnu123456@users.noreply.github.com>
2022-11-17 20:35:35 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,7 +1,6 @@
 <?php
-   include "./connection.php";
+   
    $sql = "SELECT `b m 180ml_p` FROM `shop1` WHERE date='2007-05-30';";
-   $result = $conn->query($sql);
-   $row = $result->fetch_assoc();
-   echo $row;
+   
+   echo "okkkkk";
 ?>
